88b6488f478837b9bbd8376c4fdae8458a4e889d
patrick <786156072@qq.com>
2024-04-22 14:12:55 +0800

    feat: tabs

--- app/index.jsx
+++ app/index.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
@@ -6,6 +7,7 @@ export default function Index() {
     <View className="flex-1 items-center justify-center">
       <Text className="text-3xl font-pblack">Aora!!</Text>
       <StatusBar style="auto" />
+      <Link href="/home" className="text-blue-500">Home</Link>
     </View>
   )
 }
