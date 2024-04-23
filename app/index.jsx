57fbc99d52706cfcddf16dcffdba6db594030879
patrick <786156072@qq.com>
2024-04-23 11:30:45 +0800

    fix: index page status bar issue

--- app/index.jsx
+++ app/index.jsx
@@ -8,39 +8,41 @@ import { images } from '../constants';
 
 export default function Index() {
   return (
-    <SafeAreaView className="bg-primary h-full">
-      <ScrollView contentContainerStyle={{ height: '100%' }}>
-        <View className="w-full justify-center items-center min-h-[85vh] px-4">
-          <Image
-            source={images.logo}
-            className="w-[130px] h-[84px]"
-            resizeMode="contain"
-          />
-          <Image
-            source={images.cards}
-            className="max-w-[380px] w-full h-[300px]"
-            resizeMode="contain"
-          />
-          <View className="relative mt-5">
-            <Text className="text-3xl text-white font-bold text-center">Discover Endless Possibilities with
-              <Text className="text-secondary-200"> Aora</Text>
-            </Text>
+    <>
+      <SafeAreaView className="bg-primary h-full">
+        <ScrollView contentContainerStyle={{ height: '100%' }}>
+          <View className="w-full justify-center items-center min-h-[85vh] px-4">
             <Image
-              source={images.path}
-              className="w-[136px] h-[15px] absolute -bottom-2 -right-8"
+              source={images.logo}
+              className="w-[130px] h-[84px]"
               resizeMode="contain"
             />
-          </View>
-          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">Where creativity meets innovation: embark on a journey of limitless exploration with Aora</Text>
+            <Image
+              source={images.cards}
+              className="max-w-[380px] w-full h-[300px]"
+              resizeMode="contain"
+            />
+            <View className="relative mt-5">
+              <Text className="text-3xl text-white font-bold text-center">Discover Endless Possibilities with
+                <Text className="text-secondary-200"> Aora</Text>
+              </Text>
+              <Image
+                source={images.path}
+                className="w-[136px] h-[15px] absolute -bottom-2 -right-8"
+                resizeMode="contain"
+              />
+            </View>
+            <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">Where creativity meets innovation: embark on a journey of limitless exploration with Aora</Text>
 
-          <CustomButton
-            title="Continue with Email"
-            handlePress={() => router.push('sign-in')}
-            containerStyles="w-full mt-7"
-          />
-        </View>
-      </ScrollView>
+            <CustomButton
+              title="Continue with Email"
+              handlePress={() => router.push('sign-in')}
+              containerStyles="w-full mt-7"
+            />
+          </View>
+        </ScrollView>
+      </SafeAreaView>
       <StatusBar backgroundColor='#161622' style='light' />
-    </SafeAreaView>
+    </>
   )
 }
